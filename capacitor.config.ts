6ce5f4db265f5cd3d08f02bb54e9d1877298858c
AlexisRqs <alexisrqs@outlook.fr>
2023-04-14 11:16:25 +0200

    First commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.jobmatch',
+  appName: 'jobmatch',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
